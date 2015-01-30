462ef5ef4fe8f19ac10dffd4c0cd9cf063352b28
Michael Sachs <sachsmc@gmail.com>
2015-01-30 09:09:27 -0500

    fix #22

--- R/ggroc.R
+++ R/ggroc.R
@@ -76,6 +76,7 @@ ggroc <- function(rocdata, fpf_string = "FPF", tpf_string = "TPF", ci = FALSE,
 #'   as \code{datalist}
 #' @param label.angle Adjustment for angle of label, same length as
 #'   \code{datalist}
+#'   @param plotmath Logical. If TRUE, labels will be parsed as expressions. See \code{?plotmath} for details. 
 #'   
 #' @export
 #' 
@@ -84,46 +85,57 @@ ggroc <- function(rocdata, fpf_string = "FPF", tpf_string = "TPF", ci = FALSE,
 
 multi_ggroc <- function(datalist, fpf_string = rep("FPF", length(datalist)), tpf_string = rep("TPF", length(datalist)), 
                         lty = 1:length(datalist), label = NULL, label.adj.x = rep(0, length(datalist)), 
-                        label.adj.y = rep(0, length(datalist)), label.angle = rep(45, length(datalist))){
+                        label.adj.y = rep(0, length(datalist)), label.angle = rep(45, length(datalist)), 
+                        plotmath = TRUE){
   
   stopifnot(all(sapply(1:length(datalist), function(i) fpf_string[i] %in% colnames(datalist[[i]]))))
   stopifnot(all(sapply(1:length(datalist), function(i) tpf_string[i] %in% colnames(datalist[[i]]))))
+  if(!is.null(label)){ 
+    stopifnot(length(label) == length(datalist))
+    inlabel <- as.character(label)
+    
+  } else {
+    inlabel <- LETTERS[1:length(datalist)]
+    
+  }
+  
   
   min_br <-  c(seq(0, .1, by = .01), seq(.9, 1, by = .01))
   br <- c(0, .1, .25, .5, .75, .9, 1)
   
-  p1 <- ggplot2::ggplot(datalist[[1]], ggplot2::aes_string(x = fpf_string[1], y = tpf_string[1]))  + 
+  ldatalist <- lapply(1:length(datalist), function(i){
+    
+    df <- datalist[[i]]
+    colnames(df)[colnames(df) == fpf_string[i]] <- "FPF"
+    colnames(df)[colnames(df) == tpf_string[i]] <- "TPF"
+    df$Marker <- inlabel[i]
+    df
+    
+  })
+  plotframe <- do.call(rbind, ldatalist)
+  
+  p1 <- ggplot2::ggplot(plotframe, ggplot2::aes_string(x = "FPF", y = "TPF", linetype = "Marker")) + ggplot2::geom_path() + 
     ggplot2::geom_point(color = "red", alpha = 0) +
     ggplot2::geom_abline(intercept = 0, slope = 1, lty = 1, color = "white") + 
     ggplot2::scale_x_continuous("False positive fraction", minor_breaks = min_br, breaks = br) + 
-    ggplot2::scale_y_continuous("True positive fraction", minor_breaks = min_br, breaks = br) + ggplot2::geom_path(linetype = lty[1]) 
+    ggplot2::scale_y_continuous("True positive fraction", minor_breaks = min_br, breaks = br) 
   
+   
   if(!is.null(label)){
+   
+    for(i in 1:length(label)){
     
-    xy <- datalist[[1]][datalist[[1]]$TPF + datalist[[1]]$FPF < 1, c(fpf_string[1], tpf_string[1])][1,]
-    X <- xy[1] + label.adj.x[1] + .05
-    Y <- xy[2] - .05 + label.adj.y[1]
-    p1 <- p1 + ggplot2::geom_text(data = data.frame(FPF = X, TPF = Y, label = label[1]), 
-                                  ggplot2::aes_string(x = "FPF", y  = "TPF", label = "label"), angle = label.angle[1])
-    
-  }
-  
-  for(i in 2:length(datalist)){
-    
-    p1 <- p1 + ggplot2::geom_path(data = datalist[[i]], ggplot2::aes_string(x = fpf_string[i], y = tpf_string[i]), lty = lty[i]) + 
-      ggplot2::geom_point(data = datalist[[i]], ggplot2::aes_string(x = fpf_string[i], y = tpf_string[i]), color = "red", alpha = 0)
-    
-    if(!is.null(label)){
-      
-      xy <- datalist[[i]][datalist[[i]]$TPF + datalist[[i]]$FPF < 1, c(fpf_string[i], tpf_string[i])][1,]
+      xy <- datalist[[i]][datalist[[i]][, tpf_string[i]] + datalist[[i]][, fpf_string[i]] < 1, c(fpf_string[i], tpf_string[i])][1,]
       X <- xy[1] + label.adj.x[i] + .05
       Y <- xy[2] - .05 + label.adj.y[i]
-      p1 <- p1 + ggplot2::geom_text(data = data.frame(FPF = X, TPF = Y, label = label[i]), 
-                                    ggplot2::aes_string(x = "FPF", y  = "TPF", label = "label"), angle = label.angle[i])
+      p1 <- p1 + ggplot2::geom_text(data = data.frame(FPF = X, TPF = Y, label = label[i], Marker = NA), 
+                                    ggplot2::aes_string(x = "FPF", y  = "TPF", label = "label", linetype = NULL), 
+                                    angle = label.angle[i], 
+                                    parse = plotmath)
       
-    }
     
-  }
+      }
+    }
   
   p1
   
